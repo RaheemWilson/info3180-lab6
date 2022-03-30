f8b92eddc668408c88cc2b6e37b9ca5dff0811b1
RaheemWilson <wilsonzackery16@gmail.com>
2022-03-30 15:08:44 -0500

    Added NewsList

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <script>
+import NewsList from '@/components/NewsList.vue'
+
 export default {
     data() {
         return {
             message: "Hello World! Welcome to VueJS"
         };
     },
+    components: { NewsList }
 }
 </script>
 
@@ -14,9 +17,11 @@ export default {
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
         <h1>{{ message }}</h1>
       </div>
+      <NewsList />
     </div>
 </template>
 
 <style>
+
 /* Add any component specific styles here */
 </style>
